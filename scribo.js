f5f4cc5d1eb1c00d9e8f1b0068d70e0f74ee071f
Wesley Handy <wesleylhandy@gmail.com>
2017-02-09 13:29:30 -0500

    omdb moved from imbd npm to request, logdata being saved as a stringified object, timeouts reduced

--- scribo.js
+++ scribo.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const request = require('request');
 const inquirer = require("inquirer");
 const moment = require('moment');
-const imdb = require('imdb-api');
+// const imdb = require('imdb-api');
 const Twitter = require('twitter');
 const Spotify = require('spotify');
 
@@ -32,29 +32,33 @@ function getTweets() {
 			name: 'name'
 		}
 	]).then(function(user) {
-		screenName = user.name;
+		logData.screenName = user.name;
+
 	
-		client.get('statuses/user_timeline', {screen_name: screenName}, function(error, tweets, response) {
-		  var logTimestamp = moment(new Date());
-		  var logData = [logTimestamp, 'my-tweets'];
+		client.get('statuses/user_timeline', {screen_name: logData.screenName}, function(error, tweets, response) {
+
 		  if (!error) {
 		    // console.log(tweets);
+
+		    logData.tweets = [];
 		    
 		    for (var i = 0; i < tweets.length; i++) {
-
-		    	var timestamp = moment(new Date(tweets[i].created_at));
+		    	var tweetTimestamp = moment(new Date(tweets[i].created_at));
+		    	var tweetText = tweets[i].text;
+		    	logData.tweets.push({tweetTimestamp: tweetTimestamp, tweetText: tweetText});
 		    	console.log("");
-		    	console.log(timestamp.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+		    	console.log(logData.tweets[i].tweetTimestamp.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 		    	console.log("");
-		    	console.log(tweets[i].text);
+		    	console.log(logData.tweets[i].tweetText);
 		    	console.log("");
 		    	console.log("####################");
 		    	console.log("####################");
-
-		    	logData.push(timestamp,tweets[i].text);
 		    }
+		    logData.response = 'success';
 		  } else {
-		  	logData.push("my-tweets returned an error.");
+		  	console.log("I could not retrieve any tweets for you. Did you tell me the right user-name?");
+		  	logData.error = true;
+		  	logData.errorText ="Unable to retrieve tweets for that account returned an error.";
 		  }
 		  updateLog(logData);
 		});
@@ -65,14 +69,14 @@ function postTweet() {
 
 	if(!scriboData) {
 		console.log("I am unable to understand what you want to be tweeted. Please tell me exactly what you want to say.");
-		var logTimestamp = moment(new Date());
-		var logData = [logTimestamp, 'tweet', 'No tweet data provided.'];
+		logData.error = true;
+		logData.errorText = 'No tweet data provided.';
 		updateLog(logData);
 		return;
 	}
 
-	var logTimestamp = moment(new Date());
-	var logData = [logTimestamp, 'tweet', scriboData];
+	
+	logData.tweetText = scriboData;
 	inquirer.prompt([
 		{
 			type: "confirm",
@@ -82,23 +86,28 @@ function postTweet() {
 		}
 	]).then(function(response){
 
-		logData.push("User confirmed tweet = " + response.confirm);
-		updateLog(logData);
+		logData.response = "User confirmed tweet = " + response.confirm;
+		
 		if (response.confirm) {
 
-			client.post('statuses/update', {screen_name: screenName, status: scriboData},  function(error, tweet, response) {
+			client.post('statuses/update', {status: scriboData},  function(error, tweet, response) {
 			  if(error) {
-			  	console.log(error);
+			  	logData.error = true;
+			  	logData.errorText = "Error occurred when posting tweet.";
+			  	updateLog(logData);
+			  	console.log("I could not post your tweet at this time.");
 			  	throw error;
 			  }
 			  console.log("I just tweeted for you: ");
-			  console.log(scriboData); 
+			  console.log(scriboData);
+			  logData.response = 'success'; 
 			  // console.log(response);  // Raw response object.
 			  
 			});
 		} else {
 			console.log("Okay. I won't post this tweet.");
 		}
+		updateLog(logData);
 	});	
 }
 
@@ -109,13 +118,11 @@ function getSpotify() {
 		scriboData = "'The Sign' Ace of Base";
 	}
 
-	var logTimestamp = moment(new Date());
-	var logData = [logTimestamp, 'spotify-this-song'];
-
 	Spotify.search({ type: 'track', query: scriboData}, function(err, data) {
 	    if ( err ) {
-	        console.log('Error occurred: ' + err);
-	        logData.push("Spotify Error!");
+	        console.log('I had trouble getting that information from Spotify.');
+	        logData.error = true;
+	        logData.errorText = "Spotify Error!";
 	        updateLog(logData);
 	        return;
 	    }
@@ -128,22 +135,22 @@ function getSpotify() {
 
 	    var track = sorted[0];
 
-	    var artistName = track.artists[0].name;
-	    var trackName = track.name;
-	    var previewLink = track.preview_url;
-	    var albumName = track.album.name;
+	    logData.artistName = track.artists[0].name;
+	    logData.trackName = track.name;
+	    logData.previewLink = track.preview_url;
+	    logData.albumName = track.album.name;
 
 	    console.log("");
 	    console.log("$$$$$$$$$$$$$$$$$$$$");
 	    console.log("");
-	    console.log("Track '" + trackName + ".'");
-	    console.log("Recorded by '" + artistName + ".'");
-	    console.log("On the Album '" + albumName + ".'");
-	    console.log("Preview this song at - " + previewLink);
+	    console.log("Track '" + logData.trackName + ".'");
+	    console.log("Recorded by '" + logData.artistName + ".'");
+	    console.log("On the Album '" + logData.albumName + ".'");
+	    console.log("Preview this song at - " + logData.previewLink);
 	    console.log("");
 	    console.log("$$$$$$$$$$$$$$$$$$$$");
 	    console.log("");
-	    logData.push(trackName, artistName, albumName, previewLink);
+	    logData.response = 'success';
 	    updateLog(logData);
 	});
 
@@ -155,41 +162,68 @@ function getMovie() {
 
 		scriboData = "Mr. Nobody";
 	}
-	var logTimestamp = moment(new Date());
-	var logData = [logTimestamp, 'movie-this'];
 
-	imdb.get(scriboData).then(function(things){
+	var movieReq = "http://www.omdbapi.com/?t=" + scriboData + "&y=&plot=short&tomatoes=true&r=json";
 
-    	movie = things.Movie;
+	// imdb.get(scriboData).then(function(things){
 
-    	var title = movie.title;
-    	var released = moment(new Date(movie.released));
-    	var rated = movie.rated;
-    	var rating = movie.rating;
-    	var country = movie.country;
-    	var language = movie.languages;
-    	var plot = movie.plot;
-    	var actors = movie.actors;
+	request(movieReq, function(error, response, body) {
 
-    	console.log("");
-	    console.log("$$$$$$$$$$$$$$$$$$$$");
-	    console.log("");
-	    console.log(title);
-	    console.log("Released on " + released.format("dddd, MMMM Do YYYY"));
-	    console.log("Rated " + rated);
-	    console.log("Starring " + actors);
-	    console.log(plot);
-	    console.log("IMDB User Rating (out of 10): " + rating);
-	    console.log("Languages: " + language);
-	    console.log("Produced in " + country);
-	    console.log("");
-	    console.log("$$$$$$$$$$$$$$$$$$$$");
-	    console.log("");
-	    logData.push(title, released, rated, actors, plot, rating, language, country);
-	    updateLog(logData);
-	}).catch(function(issue){
-		console.log('Access to OMDB is unavailable.');
-		logData.push("An error occured with OMDB.");
+  // If there were no errors and the response code was 200 (i.e. the request was successful)...
+		if (!error && response.statusCode === 200) {
+
+		    var movie = JSON.parse(body);
+
+	    	logData.title = movie.Title;
+	    	logData.released = moment(new Date(movie.Released));
+	    	logData.rated = movie.Rated;	    	
+	    	logData.imdbRating = movie.imdbRating;	    	
+	    	logData.country = movie.Country;	    	
+	    	logData.language = movie.Language;
+	    	logData.plot = movie.Plot;
+	    	logData.actors = movie.Actors;
+	    	logData.tomatoMeter = movie.tomatoMeter;
+	    	logData.tomatoURL = movie.tomatoURL;
+
+	    	console.log("");
+		    console.log("$$$$$$$$$$$$$$$$$$$$");
+		    console.log("");
+		    console.log("____________________");
+		    console.log("      " + logData.title);
+		    console.log("____________________");
+		    console.log("");
+		    console.log("Rated " + logData.rated);
+		    console.log("Starring " + logData.actors);
+		    console.log("");
+		    console.log("------Synopsis------");
+		    console.log(logData.plot);
+		    console.log("--------------------");
+		    console.log("");
+		    console.log("IMDB User Rating (out of 10): " + logData.imdbRating);
+		    console.log("Rotten Tomatoes Critics Rating (out of 100): " + logData.tomatoMeter);
+		    console.log(logData.tomatoURL);
+		    console.log("");
+		    console.log("Languages: " + logData.language);
+		    console.log("Produced in " + logData.country);
+		    console.log("Released on " + logData.released.format("dddd, MMMM Do YYYY"));
+		    console.log("");
+		    console.log("$$$$$$$$$$$$$$$$$$$$");
+		    console.log("");
+		    logData.response = 'success';
+		    
+		    
+		
+		} else {
+			if (JSON.parse(error) == null) {
+				console.log("When I asked about this movie, I was told that it could not be found in the database. Check your search terms and try again.");
+			} else {
+				console.log("I encountered an error with the OMDB database.");
+			}
+
+			logData.error= true;
+			logData.errorText = "An OMDB error occurred.";
+
+		}
 		updateLog(logData);
 	});
 
@@ -197,12 +231,13 @@ function getMovie() {
 }
 
 function getRandomCommand() {
-	var logTimestamp = moment(new Date());
-	var logData = [logTimestamp, 'do-what-it-says'];
 
 	fs.readFile('random.txt', 'utf-8', function(err, data) {
 		if(err){
-			console.log(err);
+			console.log('I would love to help you out, but you need to tell my creator I am missing something I really need.');
+			logData.error = true;
+			logData.errorText ='Unable to read file random.txt'
+			updateLog(logData);
 		} else {
 			// console.log(data);
 			var possibleSet = data.split("|");
@@ -212,18 +247,18 @@ function getRandomCommand() {
 			if (randomSet[1]) {
 				scriboData = randomSet[1].replace('"'/g, '');
 			}
-			logData.push(randomSet);
-			updateLog(logData);
+			logData.command = scriboCommand;
+			logData.randomSet = randomSet;
 			callCommands(randomCommand);	
 		}
 	});
 }
 
-function updateLog(values) {
+function updateLog(object) {
 	if (!fs.existsSync('log.txt')) {
-		fs.writeFileSync('log.txt', "|" + values);
+		fs.writeFileSync('log.txt', JSON.stringify(object) + ",");
 	} else {
-		fs.appendFile('log.txt', "|" + values, (err) => {
+		fs.appendFile('log.txt', JSON.stringify(object) + ",", (err) => {
   			if (err) throw err;});
 	}
 }
@@ -231,13 +266,15 @@ function updateLog(values) {
 function deleteLog() {
 	fs.unlink('log.txt', function(err){
 		if(err){
-			console.log(err);
+			console.log('I tried to delete the log, but something I cannot see is stopping me.');
+			logData.error =true;
+			logData.errorText = "Unable to delete file."
 		} else {
 			console.log("I have deleted the previous log information.");
-			var logTimestamp = moment(new Date());
-			var logData = [logTimestamp, "delete-log", "successful"];
-			updateLog(logData);
+			logData.response = "success";
+			
 		}
+		updateLog(logData);
 	});
 }
 
@@ -247,22 +284,22 @@ function callCommands(command) {
 
 		case 'tweet':
 			console.log("I'll try to tweet something for you...");
-			delay = setTimeout(postTweet,2000);
+			delay = setTimeout(postTweet,1000);
 			break;
 
 		case 'my-tweets':
 			console.log("I'm going some get some tweets for you.");
-			delay = setTimeout(getTweets, 2000);
+			delay = setTimeout(getTweets, 1000);
 			break;
 
 		case 'spotify-this-song':
 			console.log("I'm going to find song data for you.");
-			delay = setTimeout(getSpotify, 2000);
+			delay = setTimeout(getSpotify, 1000);
 			break;
 
 		case 'movie-this':
 			console.log("I'm going to find movie data for you.");
-			delay = setTimeout(getMovie, 2000);
+			delay = setTimeout(getMovie, 1000);
 			break;
 
 		case 'do-what-it-says':
@@ -278,12 +315,25 @@ function callCommands(command) {
 		default:
 			console.log("");
 			console.log("!!!!!!!!!!!!!!!!!!!!"); 
+			console.log("");
 			console.log("Can you please repeat that? I do not understand what you are requesting.");
+			console.log("You can ask me to do one of the following:");
+			console.log("");
+			console.log("'tweet' 'your-message-goes here' to post something to twitter.");
+			console.log("'my-tweets' to retrieve tweets from any twitter user.");
+			console.log("'spotify-this-song' 'song-title-of-your-choice' to retrieve data on any one song.");
+			console.log("'movie-this' 'movie-title-of-your-choice' to retreive data on any one movie.");
+			console.log("'do-what-it-says' to allow me to randomly choose a command for you.");
+			console.log("'clear-log' to reset the log.txt file - don't do this unless you are really sure!");
+			console.log("");
 			console.log("!!!!!!!!!!!!!!!!!!!!");
 			console.log("");
-			var logTimestamp = moment(new Date());
-			updateLog(logTimestamp, "User entered an invalid command.");
+			logData.error= true;
+			logData.errorText = "User entered an invalid command.";
+			updateLog(logData);
 	}
 }
 
+var logTimestamp = moment(new Date());
+var logData = {logTimestamp: logTimestamp, command: scriboCommand, error: false};
 callCommands(scriboCommand);
